6a2d7d6569567e84084cf40fce7c2d678891eb8b
add1ktion <antoine.bats@gmail.com>
2025-12-10 09:09:35 +0100

    Correction return Ex4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -11,5 +11,5 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (node == NULL)
 		return 0;
 
-	return (node->left == NULL && node->right == NULL);
+	return ((node->left == NULL) && (node->right == NULL));
 }
